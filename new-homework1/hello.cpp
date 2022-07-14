beb137993c5b120bd38d4af132d6902f811ddf07
Djon <z1henia@gmail.com>
2022-07-14 17:19:43 +0300

    remade

--- new-homework1/hello.cpp
+++ new-homework1/hello.cpp
@@ -2,8 +2,6 @@
 #include "move_me_out.h" 
 #include <iostream>
 
-main(){
-void todo_move_me_out ();
-void example_function();
-
+void todo_move_me_out() {
+	std::cout << "This function should be located in the separated source file" << std::endl;
 }
--- new-homework1/move_me_out.cpp
+++ new-homework1/move_me_out.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include "move_me_out.h"
-void todo_move_me_out() {
-	std::cout << "This function should be located in the separated source file" << std::endl;
+
+int main(){
+    std::cout << "Hello, World!" << std::endl;
+    todo_move_me_out ();
+return 0;
 }
